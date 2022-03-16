7ee37bb4293908432a86efd82d093f766a7cfb77
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-03-15 22:16:43 -0700

    aother run

--- components/Disclaimer.tsx
+++ components/Disclaimer.tsx
@@ -1,6 +1,6 @@
 
+import { Fragment, useCallback} from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { Fragment} from 'react'
 
 interface interfaceDisclaimerProps {
     openModal: any;
